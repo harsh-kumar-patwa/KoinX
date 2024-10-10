020b31201f7d25c2d2ba229537fca6a74b9a4371
Harsh Kumar <harshkumar3446@gmail.com>
2024-10-10 12:24:50 +0530

    initial setup and database connected

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log('Database Connected'))
+.catch(err=>console.log(err));
+
+app.listen(3000,()=> console.log('Server running on port 3000'));
